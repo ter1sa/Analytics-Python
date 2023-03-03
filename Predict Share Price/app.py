083f25fa9bf2c9986497d11b15df340b22c57d7b
ter1sa <102048904+ter1sa@users.noreply.github.com>
2023-03-03 12:21:43 -0500

    chore: Estimate Credit

--- Predict Share Price/app.py
+++ Predict Share Price/app.py
@@ -9,14 +9,14 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         rates = float(request.form.get("rates"))
-        # print(rates)
+        print(rates)
         model1 = joblib.load("regression")
         r1 = model1.predict([[rates]])
         model2 = joblib.load("tree")
         r2 = model2.predict([[rates]])
-        return(render_template("index.html", result1=r1, result2=r2))
+        return (render_template("index.html", result1=r1, result2=r2))
     else:
-        return(render_template("index.html", result1="waiting", result2="waiting"))
+        return (render_template("index.html", result1="waiting", result2="waiting"))
 
 
 if __name__ == "__main__":
